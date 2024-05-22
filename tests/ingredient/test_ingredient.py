e99272874151ee169eafe7b2574166e74b249f5c
Rafael <rafaelgermanio@gmail.com>
2024-05-22 08:01:34 -0300

    feat requisito de test 1 feito

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    reference = "queijo parmesão"
+    restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    ingredient = Ingredient(reference)
+    ingredient2 = Ingredient(reference)
+    assert ingredient.name == reference
+    assert ingredient.restrictions == restrictions
+    assert repr(ingredient) == "Ingredient('queijo parmesão')"
+    assert hash(ingredient) == hash(reference)
+    assert ingredient == ingredient2
